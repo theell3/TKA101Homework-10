acfe7654325e9096210bdf6fbe21e81bd1953e34
theell3 <the.ell3.com@gmail.com>
2026-04-15 16:35:27 +0800

    add hw5 q4 MyRectangle

---
+++ src/hw5/MyRectangle.java
@@ -0,0 +1,26 @@
+package hw5;
+
+public class MyRectangle {
+	
+	public MyRectangle() {}
+	
+	public MyRectangle(double width, double depth) {
+		this.width = width;
+		this.depth = depth;
+	}
+	
+	double width;
+	double depth;
+	
+	void setWidth(double width) {
+		this.width = width;
+	}
+	
+	void setDepth(double depth) {
+		this.depth = depth;
+	}
+	
+	double getArea() {
+		return width * depth;
+	}
+}
